e2f0f7ee11363f84e186d83ca4ec620634fde253
SergeyMild <sergeymild@yandex.ru>
2022-10-20 11:04:33 +0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  SafeAreaImeExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-safe-area-ime': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
